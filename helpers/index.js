7fc7bebb6484f215eb4dae3c889981b79eaa1b5b
J <j@clyne.design>
2022-05-02 12:44:31 +0400

    version update

---
+++ helpers/index.js
@@ -0,0 +1,3 @@
+export const conditionalSpread = (spread, condition) => !!condition ? spread : (Array.isArray(spread) ? [] : {});
+
+export const isExternalURL = url => !!`${url}`.match(/(https?:\/\/\S+)/g);
