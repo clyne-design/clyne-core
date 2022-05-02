e0a20e3c5af0e07a05d0d65e74d6be1b34faeda9
J <j@clyne.design>
2022-05-02 16:02:39 +0400

    version update

--- helpers/index.js
+++ helpers/index.js
@@ -3,3 +3,15 @@ export const conditionalSpread = (spread, condition) => !!condition ? spread : (
 export const isExternalURL = url => !!`${url}`.match(/(https?:\/\/\S+)/g);
 
 export const safeREL = 'noopener noreferrer';
+
+export const backgroundImage = url => conditionalSpread({
+    backgroundImage: url,
+}, url);
+
+export const fakeArray = number => Array(number).fill('').map((_, index) => index);
+
+export const rem = number => `${number / 10}rem`;
+
+export const isObject = val => typeof val === 'object' && val !== null;
+
+export const isFunction = func => typeof func === 'function';
