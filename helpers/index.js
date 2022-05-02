cab1badbaab5029f4e2441cff241572fbf9b8d52
J <j@clyne.design>
2022-05-02 12:46:43 +0400

    version update

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,5 @@
 export const conditionalSpread = (spread, condition) => !!condition ? spread : (Array.isArray(spread) ? [] : {});
 
 export const isExternalURL = url => !!`${url}`.match(/(https?:\/\/\S+)/g);
+
+export const safeREL = 'noopener noreferrer';
---
+++ props/index.js
@@ -0,0 +1,8 @@
+export const toProps = {
+    target: [
+        '_self',
+        '_blank',
+        '_parent',
+        '_top',
+    ],
+};
