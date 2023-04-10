432867b63c84b8a61b34a881fbb8dc1124c31f6e
J <j@clyne.design>
2023-04-10 20:31:05 +0400

    update

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,6 @@
 export const conditionalSpread = (spread, condition) => !!condition ? spread : (Array.isArray(spread) ? [] : {});
 
-export const isExternalURL = url => !!`${url}`.match(/(https?:\/\/\S+)/g);
+export const isExternalURL = url => !!`${url}`.match(/(https?:\/\/\S+)/g) || `${url}`.includes('mailto:') || `${url}`.includes('tel:');
 
 export const safeREL = 'noopener noreferrer';
 
